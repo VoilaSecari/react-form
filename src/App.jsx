0f9b25fa45b6d60db359833fe3ff68ea32d7a084
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-26 16:45:44 +0200

    reactive form

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,24 @@ export default function App() {
     "Titolo terzo articolo",
   ]);
   const [newArticle, setNewArticle] = useState("");
+
+  // funzione submit!
+  const handleFormSubmit = (e) => {
+    //prevenire il ricarico pagina, default
+    e.preventDefault();
+    //copia dell'array con i nuovi elementi
+    const newArticlesList = [...articlesList, newArticle];
+    //aggiornare ora la nuova lista con react e la funzione
+    setArticlesList(newArticlesList);
+    // reset blank input dopo submit
+    setNewArticle("");
+  };
+
   //creare un return html
   return (
     <main>
       <div className="container mt-3">
-        <form className="d-flex gap-4">
+        <form onSubmit={handleFormSubmit} className="d-flex gap-4">
           <input
             type="text"
             className="form-control grow-1"
@@ -21,6 +34,17 @@ export default function App() {
           />
           <button className="btn btn-primary">Aggiungi titolo</button>
         </form>
+        <h2 className="mt-5">Gli articoli presenti:</h2>
+        <hr />
+
+        {/* ora stampare la lista, mostrarla in pagina */}
+        <ul className="mt-4">
+          {articlesList.map((article, index) => (
+            <li key={index} className="mt-2">
+              {article}
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
