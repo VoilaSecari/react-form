92a8cd0bd3a7a6537152fac2e5394e84585a5b52
VoilaSecari <lifeaslaurafilip@gmail.com>
2025-05-26 16:32:44 +0200

    form creato

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 import { useState } from "react";
 
 export default function App() {
-  return <h1>La mia app 2.0</h1>;
+  //definisco gli stati per lista presente e articolo nuovo
+  const [articlesList, setArticlesList] = useState([
+    "Titolo primo articolo",
+    "Titolo secondo articolo",
+    "Titolo terzo articolo",
+  ]);
+  const [newArticle, setNewArticle] = useState("");
+  //creare un return html
+  return (
+    <main>
+      <div className="container mt-3">
+        <form className="d-flex gap-4">
+          <input
+            type="text"
+            className="form-control grow-1"
+            value={newArticle}
+            onChange={(e) => setNewArticle(e.target.value)}
+          />
+          <button className="btn btn-primary">Aggiungi titolo</button>
+        </form>
+      </div>
+    </main>
+  );
 }
